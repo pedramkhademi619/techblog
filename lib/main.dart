77b750fe557ec07364b910e685e2644f856fb205
pedramkhademi619 <pedramkhademi619@gmail.com>
2025-01-10 22:56:37 +0330

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:tech/view/main_screen.dart';
 import 'package:tech/view/my_cats.dart';
 
 import 'my_colors.dart';
